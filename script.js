66c882615520a4f8fd23b527507fdcd91e9c5eec
Esme577 <esmeraldaquezada57@gmail.com>
2025-04-03 16:30:49 -0700

    I added a style css file and commented out and labled in my html file what btn where for just to make it easy when I uncomment out my comments in js file

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 //event triggered when page is fully loaded
-document.addEventListener("DOMContentLoaded", function(){
+//document.addEventListener("DOMContentLoaded", function(){
     let onLoadElemnt = document.getElementById("onload");
     onLoadElemnt.innerHTML = "<h1> I loaded because the DOM was fully rendered</h1>"
-}
-)
+//}
+//)
 //keyevent
-let directionElement = document.getElementById("direction");
+//let directionElement = document.getElementById("direction");
 //event triggers when a key is pressed
-document.addEventListener("keydown", function (e){
-    directionElement.innerHTML = `The key you presssed is ${e.code}`
-})
+//document.addEventListener("keydown", function (e){
+//    directionElement.innerHTML = `The key you presssed is ${e.code}`
+//})
 
 //clickevent
 
-let clickMeElement = document.getElementById("clickMe");
-document.addEventListener("click", function (e){
+//let clickMeElement = document.getElementById("clickMe");
+//document.addEventListener("click", function (e){
 
-    clickMeElement.innerHTML = "<h1> you have clicked me!/h1>"
-})
+//    clickMeElement.innerHTML = "<h1> you have clicked me!/h1>"
+//})
 
 //background random colorchange
 
-function changeinBackgroundColor(){
-    let RandomRGB = ` rgb(${Math.floor(Math.random() * 256)},
-    ${Math.floor(Math.random() * 256)},
-    ${Math.floor(Math.random() * 256)})`; // there are 3 because there are red blue yellowand 256 becasue each have that amount of possibilities
-    document.body.style.backgroundColor = RandomRGB;
+//function changeinBackgroundColor(){
+//    let RandomRGB = ` rgb(${Math.floor(Math.random() * 256)},
+//    ${Math.floor(Math.random() * 256)},
+//    ${Math.floor(Math.random() * 256)})`; // there are 3 because there are red blue yellowand 256 becasue each have that amount of possibilities
+//    document.body.style.backgroundColor = RandomRGB;
 
-    document.body.style.backgroundColor = RandomRGB;
-}
-let randomColorBtnElement = document.getElementById("randomColorBtn");
-randomColorBtnElement.addEventListener("click", changeinBackgroundColor);
+//    document.body.style.backgroundColor = RandomRGB;
+//}
+//let randomColorBtnElement = document.getElementById("randomColorBtn");
+//randomColorBtnElement.addEventListener("click", changeinBackgroundColor);
+
+
+//classList Toggle Demo 
+function changeBtnStyle() {
+    const btn = document.querySelector(".cool-btn")
+    btn.classList.toggle("glow"); // Toggles glow effect
+   }
+   
