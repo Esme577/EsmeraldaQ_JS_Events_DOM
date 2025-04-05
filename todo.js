42aee54b2eaab90506d9673e0da0f6a50f286392
Esme577 <esmeraldaquezada57@gmail.com>
2025-04-04 17:52:19 -0700

    I fixed  some errors as i had spelled in the task function the value I had not wrote it correctly I also added taskTest in the paranthese in the function createListItem as what it does is the vlaue that the use inputs through taskTextand once its inputed the function than puts it into a bullet point  we also in the function below is is assign taskTest to be the variable so that means that when somone inputs something its saved to the taskTest variable that gets trim on the sides from spacing, then we have an if statement that basicly checks if somethng was written go foward if not dont do anything  and if something is wriiten we asign it the varibale listItem that is equal to the createListItem(taskTest) which sends it to the function createListItem and then we allow it to be put into that list by putting an append we then clear our value so that the user can continue to add more items to the list. I then compied how i had done the toggle from my other excrecise changed some words so that I can have a green background and looked up how to cross out and addd that to my css file and its done

--- todo.js
+++ todo.js
@@ -2,14 +2,14 @@ const taskInput = document.querySelector("#taskInput");
 const addTaskButton = document.querySelector("#addTask");
 const taskList = document.querySelector("#taskList");
 
-function createListItem(){
+function createListItem(taskText){
     const listItem = document.createElement("li");
     listItem.textContent = taskText;
-    return listItem
+    return listItem;
 }
 
 function addTask(){
-    const taskText = taskInput.ariaValueMax.trim();
+    const taskText = taskInput.value.trim();
 
     if(taskText !== ""){
         const listItem = createListItem(taskText);
@@ -18,3 +18,9 @@ function addTask(){
     }
 }
 addTaskButton.addEventListener("click",addTask);
+
+//Add toggle with greenbackground and crossoutfunction when task is completed
+function taskComplete() {
+   const btn = document.querySelector("li")
+    btn.classList.toggle("complete"); // Toggles glow effect
+}
